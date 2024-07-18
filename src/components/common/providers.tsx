8a02d8ebd84b6fb6748e8eb2142e8d13ed37552e
Hoaxnt <marcjulius.regacho@cvsu.edu.ph>
2024-07-18 11:37:03 +0800

    initial commit

--- src/components/common/providers.tsx
+++ src/components/common/providers.tsx
@@ -3,6 +3,16 @@
 import { SessionProvider } from "next-auth/react";
 import { PropsWithChildren } from "react";
 
+// core styles are required for all packages
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import '@mantine/dropzone/styles.css';
+import '@mantine/carousel/styles.css';
+import '@mantine/modals/styles.css';
+
+import { createTheme, MantineProvider } from '@mantine/core';
+
+
 export const Providers = ({ children }: PropsWithChildren<{}>) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
